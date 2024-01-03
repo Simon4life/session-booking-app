ee025a80ff7c01accd8e68ca4b8cce2782c01f4b
Simon Ogar <ogarsimon0000@gmail.com>
2024-01-03 23:36:30 +0100

    intial commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import { ComponentPropsWithoutRef } from "react";
+import { Link } from "react-router-dom";
+
+type Anchor = ComponentPropsWithoutRef<typeof Link> & {
+    to?: string
+}
+
+type Button = ComponentPropsWithoutRef<"button"> & {
+    to?: never
+}
+
+const isLink = (props: Anchor | Button): props is Anchor => {
+    return "to" in props
+}
+
+const Button = (props: Anchor | Button) => {
+    if(isLink(props)) {
+        return <Link className="button" {...props}>click Me</Link>
+    }
+    return <button className="button">Click me</button>
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <div id="main-header">
+            <h1>Booking Session</h1>
+            <nav>
+                <ul>
+                    <li><a>link 1</a></li>
+                    <li><a>link 1</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
